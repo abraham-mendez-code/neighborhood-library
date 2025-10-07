5aa81ddffdad6dd183b98fd8a019d6cb2cc56543
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-06 21:33:33 -0400

    Added show method to display CheckOut menu and options

--- src/main/java/com/pluralsight/CheckedOutBooks.java
+++ src/main/java/com/pluralsight/CheckedOutBooks.java
@@ -26,15 +26,15 @@ public class CheckedOutBooks {
                     // clear buffer
                     input.nextLine();
 
-                    System.out.println("Select a book");
-                    String selection = input.nextLine();
+                    System.out.println("Enter the ID");
+                    int id = input.nextInt();
 
-                    validated = findSelection(selection, books);
+                    validated = findSelection(id, books);
                     break;
                 case 'x':
                     return;
                 default:
-                    System.out.println("Invalid input detected. Try (1 or 2)");
+                    System.out.println("Invalid input detected. Try (C or X)");
 
             }
         }
@@ -43,13 +43,13 @@ public class CheckedOutBooks {
 
     // this method shows available books
     private static void show(Book[] books) {
-        System.out.println("Available books");
+        System.out.println("Checked Out books");
 
         // loop through all books
         for(int i = 0; i < books.length; i++) {
             // if the book is not checked out
-            if (!books[i].isCheckedOut()) {
-                System.out.printf("ID: %d, ISBN: %s, Title: %s\n", books[i].getId(), books[i].getIsbn(), books[i].getTitle());
+            if (books[i].isCheckedOut()) {
+                System.out.printf("ID: %d, ISBN: %s, Title: %s, is checked out by %s\n", books[i].getId(), books[i].getIsbn(), books[i].getTitle(), books[i].getCheckedOutTo());
             }
 
         }
@@ -57,7 +57,7 @@ public class CheckedOutBooks {
 
     // this method finds a selected book and checks it out, then returns a boolean,
     // if operation was successful bool = true, else bool = false
-    private static boolean findSelection (String selection, Book[] books) {
+    private static boolean findSelection (int id, Book[] books) {
 
         // check each book
         for (int i = 0; i < books.length; i++) {
